91daefb3511677ebccc39b008c92f82cd51f978d
purna khatri <purnakhatri630@gmail.com>
2024-11-23 15:37:50 +0400

    number of month

---
+++ number_of_month.py
@@ -0,0 +1,57 @@
+
+number_of_month = ["Jajanuary","february","march","april","may","june","july","august""september""october""november","december"]
+
+
+current_month = input("Enter the current month : ").capitalize()
+
+if current_month == 'January':
+
+    print(f"This month is january")
+
+elif current_month == 'February':
+
+    print(f"This month is February")
+
+elif current_month == 'March':
+
+    print(f"This Month is March")
+
+elif current_month == 'April':
+
+    print(f"This month is April")
+
+elif current_month == 'May':
+
+    print(f"This month is May")
+
+elif current_month == 'June':
+
+    print(f"This Month is june")
+
+elif current_month == 'July':
+
+    print(f"This month is July")
+
+elif current_month == "August":
+
+    print(f"This month is August")
+
+elif current_month == "September":
+
+    print(f"This month is September")
+
+elif current_month  == 'October':
+
+    print(f"This month is October")
+
+elif current_month == 'November':
+
+    print(f"This month is November")
+
+elif current_month == 'December':
+
+    print(f"This Month is December")
+
+else:
+
+    print(f"invalid Month")
