ce6d63b0bb4e74560eeac7a26fc3318b2d5401e6
purna khatri <purnakhatri630@gmail.com>
2024-11-23 13:47:01 +0400

    traffice light

---
+++ traffice_light.py
@@ -0,0 +1,19 @@
+traffice_light = ["Red", "Green", "Yellow"]
+
+current_traffice_light = input("Enter The Current Traffice Light : ").capitalize()
+
+if current_traffice_light == "Red":
+
+    print(f"Please Stop Here Because The Traffice Light is Red !")
+
+elif current_traffice_light == "Green":
+
+    print(f"Please You Can Through Here Because The Traffice Light is Green :")
+
+elif current_traffice_light == "Yellow":
+
+    print(f"As the traffic light is yellow please go slow from here")
+
+else:
+
+    print(f"Here is the traffic light damage you can stay here")
