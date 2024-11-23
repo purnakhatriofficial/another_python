8d406551291fa5e104aa54577de89d182f3c5068
purna khatri <purnakhatri630@gmail.com>
2024-11-23 15:09:30 +0400

    number of days

---
+++ number_of_days.py
@@ -0,0 +1,36 @@
+number_of_days = int(input("Enter the number : "))
+
+
+if number_of_days ==1:
+
+    print(f"Sunday")
+
+elif number_of_days == 2:
+
+    print(f"Monday")
+elif number_of_days ==3:
+    
+    print(f"Tuesday")
+
+elif number_of_days == 4:
+
+    print(f"Wednesday")
+
+elif number_of_days == 5:
+
+    print(f"Thrusday")
+
+elif number_of_days == 6:
+
+    print(f"Friday")
+
+elif number_of_days == 7:
+
+    print(f"Saturday")
+
+else:
+
+    print(f"Invalid Days")
+
+
+
