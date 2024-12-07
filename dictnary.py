8cf6b7a6fe65d79ad859d7689baef71ee1e70804
purna khatri <purnakhatri630@gmail.com>
2024-12-07 23:23:14 +0400

    dictonary

---
+++ dictnary.py
@@ -0,0 +1,7 @@
+#Data Dictnorary
+
+student_name = {'name':  'purna Bahadur Khatri', 'age': " i am 25 years old ", 'my gamil account is ': 'purnakhatri630@gmail.com', 'course': [ ' C+', "java", "web", "App"]}
+
+
+for key, values in student_name.items():
+    print(key , values)
